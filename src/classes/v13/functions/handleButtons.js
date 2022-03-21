ef92d828af336ec5e6a452d6b0152a3fe155b125
lightninbolt986 <raisinghani.armaan1@gmail.com>
2022-03-21 12:33:19 +0530

    e

--- src/classes/v13/functions/handleButtons.js
+++ src/classes/v13/functions/handleButtons.js
@@ -48,7 +48,7 @@ module.exports = async (client, button) => {
 				if(data.requirements.roles) {
 					const roles = data.requirements.roles.map(x => button.message.guild.members.cache.get(button.user.id).roles.cache.get(x));
 					console.log(roles)
-					if (!roles[0] && button.message.member.roles.cache.some(e=> e.id == '954013633711583262')) {
+					if (!roles[0] && button.message.member.roles.cache.some(e=> e.id == '858901404507963403') && button.message.member.roles.cache.some(e=> e.id == '953949479227232326') && button.message.member.roles.cache.some(e=> e.id == '855869393865932800')) {
 						const requiredRoles = button.message.guild.roles.cache.filter(x => data.requirements.roles.includes(x.id)).filter(x => !button.message.guild.members.cache.get(button.user.id).roles.cache.get(x.id)).map(x => `\`${x.name}\``).join(', ');
 						return button.reply({ content: client.customMessages.giveawayMessages.nonoRole.replace(/{requiredRoles}/g, requiredRoles), ephemeral : true });
 					}
